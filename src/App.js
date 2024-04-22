1d3c886bf2507560b6535073ee10770fadc27f4b
herbertqw <herbert@o3.network>
2024-04-22 12:03:14 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <p>
+          对接以下接口，并实现一个方法可以根据任意id获取到任意节点数据。
+          https://base.mfdemo.cn/boot/sys/column/tree?projectId=39
+        </p>
+        <p>
+          动画特效方向测试题：
+          选取以下网站中任意两个动画效果复现，越简单的动画要求还原度越高。
+          https://mfsunny.com/
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
